b37e7692241fca05b7a823cfe4d07412473015cc
Charlotte Hallisey <charlotte.hallisey@duke.edu>
2021-11-12 00:53:13 -0500

    initial structure set up

---
+++ ACLApp/ACLApp/ViewControllers/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ACLApp
+//
+//  Created by  Charlotte Hallisey on 11/11/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
