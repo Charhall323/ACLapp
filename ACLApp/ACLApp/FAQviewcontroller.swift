6dd77efd34a5419892ca73d41bf195811dd26a97
Charlotte Hallisey <charlotte.hallisey@duke.edu>
2021-11-30 12:55:39 -0500

    logo and FAQ and lifestyle edit

--- ACLApp/ACLApp/FAQviewcontroller.swift
+++ ACLApp/ACLApp/FAQviewcontroller.swift
@@ -65,3 +65,9 @@ struct ProfileSummary: View {
 //            .environmentObject(ModelData())
 //    }
 //}
+
+struct FAQviewcontroller_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
