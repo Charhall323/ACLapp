212dae6bd1ccf51d2fc8563addc99d11dc178666
Charlotte Hallisey <charlotte.hallisey@duke.edu>
2021-12-05 21:56:04 -0500

    grade 1 exercise videos working

--- ACLApp/ACLApp/AppDelegate.swift
+++ ACLApp/ACLApp/AppDelegate.swift
@@ -5,6 +5,7 @@
 
 import UIKit
 import Firebase
+import AVFoundation
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
+        let audioSession = AVAudioSession.sharedInstance()
+            do {
+                try audioSession.setCategory(.playback, mode: .moviePlayback)
+            }
+            catch {
+                print("Setting category to AVAudioSessionCategoryPlayback failed.")
+            }
+            return true
         return true
     }
 
