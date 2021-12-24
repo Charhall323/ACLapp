259891882d9fcb0884f8e8adfb1fc2727aecae79
Charlotte Hallisey <charlotte.hallisey@duke.edu>
2021-12-23 23:32:06 -0500

    80% part fixed, automatically logged (grade 3 and grade 1 still flopped but properly accounted for), connected to firebase with the three childeren, still have to fix error on creating the progress calendar page 70% done and log in fix next

---
+++ ACLApp/ACLApp/Helpers/PlayerHelper.swift
@@ -0,0 +1,8 @@
+//
+//  PlayerHelper.swift
+//  ACLApp
+//
+//  Created by  Charlotte Hallisey on 12/23/21.
+//
+
+import Foundation
---
+++ ACLApp/ACLApp/Objects/EventObjects.swift
@@ -0,0 +1,8 @@
+//
+//  EventObjects.swift
+//  ACLApp
+//
+//  Created by  Charlotte Hallisey on 12/23/21.
+//
+
+import Foundation
