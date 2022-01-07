830ed278c1cea4059fbb3c165bc7ab2d73801014
Charlotte Hallisey <charlotte.hallisey@duke.edu>
2022-01-06 23:44:33 -0500

    verification now requires email, progress the same because verification fixing fixed it

--- ACLApp/ACLApp/ViewControllers/LoginViewController.swift
+++ ACLApp/ACLApp/ViewControllers/LoginViewController.swift
@@ -34,7 +34,7 @@ class LoginViewController: UIViewController {
             
             //if the users password does not follow the constraints, make blank
             //? = optional variable means the variable does not need to be initialized when you defiine it, if you do not initialize it with a default value then its default value is null
-            if let error = error as NSError? {
+            if let error = error as NSError? { //means the error is not null, so got an error back and bad log in
             switch AuthErrorCode(rawValue: error.code) {
             default:
                 print("Error: \(error.localizedDescription)")
@@ -42,14 +42,44 @@ class LoginViewController: UIViewController {
                 self.passwordTextfield.text = "" //delete letters typed
             }
             //if the users password does follow the constraints, sign the user in and go to the initation page
-          } else {
-                print("User signed in successfully")
+          } else { //no error so now check if actually verified
+//                print("User signed in successfully")
                 let userInfo = Auth.auth().currentUser //logs user info
+              
+              //changed this so now it checks to see if the user email is verified, and only once it is verified then it will take the user to the main page
+              if (userInfo!.isEmailVerified) { //checking now signed in and email is a legitimate user is the email verified
+                  //only once the user is verified, makes sure user is actually verified, has to check if the email is actually verified
                 let storyboard = UIStoryboard(name: "Main", bundle: nil) //goes to storyboard
                 let vc = storyboard.instantiateViewController(withIdentifier: "TabController") //goes to specific view controller
                 self.present(vc, animated: true) //presents correct view
+              }
+              else {
+                  self.showEmailNotVerifiedAlert(authUser: userInfo) //if not verified see alert
+              }
           }
         }
+    }
+    //special alert if the email is not verified to see if need to send another verification email
+    func showEmailNotVerifiedAlert(authUser: User?) { //shows a specialized alert for the email not verified
+        let alert = UIAlertController(title: "Email not Verified", message: "Please verify your email", preferredStyle: .alert) //creates the alert
+        alert.addAction(UIAlertAction(title: "Resend email", style: .default, handler: { action in
+            authUser!.sendEmailVerification(completion: { (error) in //creates a button in the alert to resend the verification email
+                if error == nil {
+                    self.showAlert(title: "Verification Email Sent!", message: "Please check your email for a verification email and once verified, feel free to use the log in option below") }
+                else {
+                    self.showAlert(title: "Error", message: "Invalid Email")
+                }
+                })
+                }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
+                                            
+    
+    func showAlert(title: String, message: String) { //used to just be able to show an alert to the user (ex: invalid username/password, incorrect email, cannot log in etc)
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert) //create error message
+        self.present(alert, animated: true, completion: nil) //actually shows message
+        Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false, block: {_ in alert.dismiss(animated: true, completion: nil)}) //alert shown (alert removed after error message has been seen (3 seconds interval)
     }
     
 
--- ACLApp/ACLApp/ViewControllers/SignUpViewController.swift
+++ ACLApp/ACLApp/ViewControllers/SignUpViewController.swift
@@ -27,11 +27,12 @@ class SignUpViewController: UIViewController {
                 if let error = error as NSError? { //if there is an error in the password (does not follow designated parameters)
                 switch AuthErrorCode(rawValue: error.code) { //then there is an error
                 default:
-                    self.showAlert(message: "Cannot create user") //the password cannot be created, alert shown (calls function below showing error message)
+                    self.showAlert(title: "error", message: "Cannot create user") //the password cannot be created, alert shown (calls function below showing error message)
                 }
               } else {
                   self.sendVerificationMail() //if the password works have to check if email passes
 //                print("User signed up successfully")
+                  //took out what was below because that is what was taking it to the main view controller 
 //                  let storyboard = UIStoryboard(name: "Main", bundle: nil)
 //                  let vc = storyboard.instantiateViewController(withIdentifier: "TabController")
 //                  self.present(vc, animated: true)
@@ -51,29 +52,37 @@ class SignUpViewController: UIViewController {
     
     func sendVerificationMail() { //checking if can send a verification email
         let authUser = Auth.auth().currentUser //identifying the the current user
-        if authUser != nil && !authUser!.isEmailVerified { //if the user can send an email
-            authUser!.sendEmailVerification(completion: { (error) in //try to send the email to user
+        if authUser != nil && !authUser!.isEmailVerified { //if the user can send an email (if this is the current user) but the email has not been verified yet 
+            //isemailverified = a function for authentication that returns true if the user has verified their email
+            authUser!.sendEmailVerification(completion: { (error) in //tries to sends email verification
+                //try to send the email to user
                 //notify the user that the mail has send or could nbot have because of an error
-                if error == nil { //if the email can be sent so there is no error then the email is valid
-                    print("User signed up successfully") //the user is successfully signed in
-                    let storyboard = UIStoryboard(name: "Main", bundle: nil) //show the initial view controller
-                    let vc = storyboard.instantiateViewController(withIdentifier: "TabController") //showing specific page
-                    self.present(vc, animated: true) //show that page
+                if error == nil { //if the email can be sent that tells you that the email is real
+                    //now below, wait to email goes through and is verified for them to verify and show log in screen
+                    self.showAlert(title: "Verification Email Sent!", message: "Please check your verification email and once verified, feel free to use the log in option below") //message to user that when sign up send you a verification email now, once you sign up never logged in until go to log in and put in your credentials after verified
+                    
+                    
+                    //click sign up once do that sends emails and once verified email click log in and it will log you in
+                    
+                    
+//                    let storyboard = UIStoryboard(name: "Main", bundle: nil) //show the initial view controller
+//                    let vc = storyboard.instantiateViewController(withIdentifier: "TabController") //showing specific page
+//                    self.present(vc, animated: true) //show that page
                 }
                 else
                 {
-                    self.showAlert(message: "Invalid Email") //if the verification email cannot be sent then the email is not real, show message saying invalid email), calls show Alert method to show email does not work
+                    self.showAlert(title: "error", message: "Invalid Email") //if the verification email cannot be sent then the email is not real, show message saying invalid email), calls show Alert method to show email does not work
                 }
             })
         }
         else {
-            //either the user is not available, or the user is already verified
+            //either the user is not available, or the user is already verified (would not happend because then would either be logged in or just there would be no error)
                             
         }
     }
     
-    func showAlert(message: String) { //used to just be able to show an alert to the user (ex: invalid username/password, incorrect email, cannot log in etc)
-        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert) //create error message
+    func showAlert(title: String, message: String) { //used to just be able to show an alert to the user (ex: invalid username/password, incorrect email, cannot log in etc)
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert) //create error message
         self.present(alert, animated: true, completion: nil) //actually shows message
         Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false, block: {_ in alert.dismiss(animated: true, completion: nil)}) //alert shown (alert removed after error message has been seen (3 seconds interval)
     }
